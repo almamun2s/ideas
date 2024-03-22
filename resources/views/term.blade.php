be595959e04aa1b51b09c54d7635e0fdfc1dc1b5
Abdullah Almamun <mamun2social@gmail.com>
2024-03-22 13:12:43 +0600

    retrive data from .env, config and made templates

---
+++ resources/views/term.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+@section('content')
+    <h2>Terms</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus asperiores eum ipsam maiores fugiat quam error
+        voluptatum accusantium aspernatur numquam. Maxime eaque dolorem laboriosam architecto, maiores cumque sapiente
+        facere! Cum.</p>
+@endsection
