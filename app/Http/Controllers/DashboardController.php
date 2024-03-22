cfb2e21202b63322e5128db65878cb325b774a03
Abdullah Almamun <mamun2social@gmail.com>
2024-03-22 23:39:04 +0600

    ideas can be get from database

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard');
+        $idea = new Idea([
+            'content' => 'Test from idea'
+        ]);
+        // dd(Idea::all());
+        // $idea->content = "Test idea";
+        // $idea->save();
+        return view('dashboard', [
+            'ideas' => Idea::all()
+        ]);
     }
 }
