42e3809f79b0e3d2f76a7d477211518389e4d598
Abdullah Almamun <mamun2social@gmail.com>
2024-03-23 22:02:36 +0600

    validation works now

--- resources/views/shared/idea_submit.blade.php
+++ resources/views/shared/idea_submit.blade.php
@@ -3,7 +3,10 @@
     <form action="{{ route('idea') }} " method="post">
         @csrf
         <div class="mb-3">
-            <textarea name="idea" class="form-control" id="idea" rows="3"></textarea>
+            <textarea name="idea" class="form-control" id="idea" rows="3" style="resize: none;" ></textarea>
+            @error('idea')
+                <span class="text-danger d-block mt-2" > {{ $message }} </span>
+            @enderror
         </div>
         <div class="">
             <button type="submit" class="btn btn-dark"> Share </button>
