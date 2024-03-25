72a6469a091152a86fb278065cdfd09b9da34945
Abdullah Almamun <mamun2social@gmail.com>
2024-03-25 13:42:59 +0600

    search works now

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,9 +8,15 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
+        $idea = Idea::orderBy('id', 'desc');
+
+        if (request()->has('search')) {
+            $idea = $idea->where('content', 'like', '%' . request()->get('search', '') .'%' );
+        }
+
         return view('dashboard', [
             // 'ideas' => Idea::orderBy('id', 'desc')->get(),
-            'ideas' => Idea::orderBy('id', 'desc')->paginate(5),
+            'ideas' => $idea->paginate(5),
         ]);
     }
 }
