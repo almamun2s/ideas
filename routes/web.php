7472e3ef6a103b911a89dab961f2fdacf36fe38b
Abdullah Almamun <mamun2social@gmail.com>
2024-03-25 22:34:44 +0600

    resources routing

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::group(['prefix' => 'ideas/', 'as' => 'ideas.', 'middleware' => ['auth'] ], function () {
-    Route::post('', [IdeaController::class, 'store'])->name('store');
-    Route::get('{idea}', [IdeaController::class, 'show'])->name('show')->withoutMiddleware('auth');
-    Route::get('{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
-    Route::put('{idea}', [IdeaController::class, 'update'])->name('update');
-    Route::delete('{idea}', [IdeaController::class, 'destroy'])->name('destroy');
+// For commenting on a post/idea
+Route::resource('ideas.comment', CommentController::class)->only(['store'])->middleware('auth');
 
-    // For commenting on a post/idea
-    Route::post('{idea}/comment', [CommentController::class, 'store'])->name('comment.store');
-});
+Route::resource('ideas', IdeaController::class )->except(['index', 'create', 'show' ])->middleware('auth');
+Route::resource('ideas', IdeaController::class )->only(['show' ]);
 
 // For User information Routes are in authRoute.php file
 
