c5a5ad567bfab8421e19e430771fb24ca9de8b96
Abdullah Almamun <mamun2social@gmail.com>
2024-03-29 00:51:15 +0600

    Admin page and middlewares added

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout')
+
+@section('content')
+    <h1>Admin Panel</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\FeedController;
 use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\IdeaController;
@@ -48,3 +49,7 @@ Route::get('/feed', FeedController::class )->middleware('auth')->name('feed');
 Route::get('/terms', function () {
     return view('term');
 })->name('terms');
+
+
+// Routes for Admins
+Route::get('/admin', [AdminDashboardController::class, 'index'])->middleware(['auth', 'admin'])->name('admin.dashboard');
